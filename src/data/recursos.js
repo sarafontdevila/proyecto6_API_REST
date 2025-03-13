e39dcb15e95f9d18b14ff92f590369b88c749cf9
sarafontdevila <146539364+sarafontdevila@users.noreply.github.com>
2025-03-13 18:07:47 +0100

    Create recursos.js

---
+++ src/data/recursos.js
@@ -0,0 +1,7 @@
+/*const recursos = [
+  {
+    nombre: "visuals",
+    imagen: "https://example.com/visuals.jpg",
+    apps: [{ type: mongoose.Types.ObjectId, ref: "apps", required: false }]
+  }
+];*/
