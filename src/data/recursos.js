43e3bd41ad0545d4174c99c5448e5d2b812d0a86
sarafontdevila <146539364+sarafontdevila@users.noreply.github.com>
2025-03-14 21:49:20 +0100

    Seeds Apps & Recursos

--- src/data/recursos.js
+++ src/data/recursos.js
@@ -1,7 +1,15 @@
-/*const recursos = [
+const apps = require("../data/apps")
+
+const recursos = [
   {
-    nombre: "visuals",
+    nombre: "Visuales",
     imagen: "https://example.com/visuals.jpg",
-    apps: [{ type: mongoose.Types.ObjectId, ref: "apps", required: false }]
-  }
-];*/
+    apps: [apps[0]._id, apps[1]._id, apps[2]._id, apps[3]._id]
+  },
+  {
+    nombre: "Audio",
+    imagen: "https://example.com/visuals.jpg",
+    apps: [apps[4]._id, apps[5]._id]
+  },
+]
+module.exports = recursos
